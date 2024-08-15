9a05be4da7ca2b601d4adf0919057bbdbe0370e1
DhuumMe <benoit.abelard@gmail.com>
2024-08-15 09:25:50 +0200

    Initial commit

---
+++ src/main/java/net/dhuumme/advanced/AdvancedClient.java
@@ -0,0 +1,10 @@
+package net.dhuumme.advanced;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdvancedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

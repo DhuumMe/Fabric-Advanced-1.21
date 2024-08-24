96cc22fc098664a11b031feba3f13553edfb2cd1
DhuumMe <benoit.abelard@gmail.com>
2024-08-24 18:30:26 +0200

    Added more water & fog colors ModDifficulty changes

--- src/main/java/net/dhuumme/advanced/difficulty/ModDifficulty.java
+++ src/main/java/net/dhuumme/advanced/difficulty/ModDifficulty.java
@@ -3,11 +3,13 @@ package net.dhuumme.advanced.difficulty;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 
 public class ModDifficulty {
+
+    private static boolean isNight = false;
+    private static boolean isDay = false;
 
     public static void init() {
         // Inscrire un événement pour chaque tick du serveur
@@ -15,22 +17,26 @@ public class ModDifficulty {
     }
 
     private static void onServerTick(MinecraftServer server) {
-        for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
-            updateDifficulty(server, player.getWorld());
-        }
+        updateDifficulty(server, server.getOverworld()); // Vérifie uniquement l'Overworld
     }
 
     private static void updateDifficulty(MinecraftServer server, World world) {
         long timeOfDay = world.getTimeOfDay() % 24000; // Récupère le temps actuel dans le cycle de 24 000 ticks
 
-        if (timeOfDay == 13000) { // C'est la nuit qui commence
-            executeCommand(server, "say §4La nuit est tombée");
-            executeCommand(server, "say Les nuits sont difficiles");
-            server.setDifficulty(Difficulty.HARD, true);
-        } else if (timeOfDay == 23000) { // C'est le jour qui commence
-            executeCommand(server, "say §eLe jour vient de se lever!");
-            executeCommand(server, "say Encore une journée normale");
-            server.setDifficulty(Difficulty.NORMAL, true);
+        if (timeOfDay >= 14000 && timeOfDay < 22000) { // C'est la nuit
+            if (!isNight) { // Si la nuit commence
+                executeCommand(server, "title @a actionbar {\"text\":\"La nuit s'annonce difficile\",\"color\":\"dark_red\"}");
+                server.setDifficulty(Difficulty.HARD, true);
+                isNight = true;
+                isDay = false; // Réinitialiser l'état du jour
+            }
+        } else { // C'est le jour
+            if (!isDay) { // Si le jour commence
+                executeCommand(server, "title @a actionbar {\"text\":\"Tout semble normal aujourd'hui\",\"color\":\"gold\"}");
+                server.setDifficulty(Difficulty.NORMAL, true);
+                isDay = true;
+                isNight = false; // Réinitialiser l'état de la nuit
+            }
         }
     }
 
@@ -39,4 +45,3 @@ public class ModDifficulty {
         server.getCommandManager().executeWithPrefix(commandSource, command);
     }
 }
-
